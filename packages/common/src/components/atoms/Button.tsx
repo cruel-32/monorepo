8a7d7d865d9cb31efa5863ae60d6a7ecabdc38e0
ITO_MZ_CHOESEUN\mz_choeseunghui <mz_choeseunghui@woowafriends.com>
2024-06-24 16:57:46 +0900

    모노레포

---
+++ packages/common/src/components/atoms/Button.tsx
@@ -0,0 +1,30 @@
+import { DetailedHTMLProps, ButtonHTMLAttributes } from 'react';
+import classnames from 'classnames';
+
+export interface ButtonProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>{
+  buttonType?: Colors;
+  className?: string;
+  size?: 'small' | 'medium' | 'large';
+  label: string;
+  isLoading?: boolean;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+}
+
+export const Button = ({
+  buttonType = 'primary',
+  size = 'medium',
+  isLoading = false,
+  label,
+  className,
+  ...props
+}: ButtonProps) => {
+  return (
+    <button
+      type="button"
+      className={classnames('btns', buttonType)}
+      {...props}
+    >
+      {label}
+    </button>
+  );
+};
--- packages/react-zustand/src/App.tsx
+++ packages/react-zustand/src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss'
+import { Button } from '@monorepo/common/src/components/atoms/Button';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Button label="Button" buttonType="primary" />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
