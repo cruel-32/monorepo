950e93e802ff5a5b40c532bc8bd573e89e63693e
ITO_MZ_CHOESEUN\mz_choeseunghui <mz_choeseunghui@woowafriends.com>
2024-07-01 14:33:09 +0900

    공통 eslint prettier

--- packages/common/src/components/atoms/Button.tsx
+++ packages/common/src/components/atoms/Button.tsx
@@ -1,7 +1,8 @@
 import { DetailedHTMLProps, ButtonHTMLAttributes } from 'react';
 import classnames from 'classnames';
 
-export interface ButtonProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
+export interface ButtonProps
+  extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
   buttonType?: Colors;
   className?: string;
   size?: 'small' | 'medium' | 'large';
@@ -10,7 +11,15 @@ export interface ButtonProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTML
   onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 }
 
-export const Button = ({ buttonType = 'primary', size = 'medium', isLoading = false, label, className, children, ...props }: ButtonProps) => {
+export const Button = ({
+  buttonType = 'primary',
+  size = 'medium',
+  isLoading = false,
+  label,
+  className,
+  children,
+  ...props
+}: ButtonProps) => {
   return (
     <button type="button" className={classnames('btns', buttonType)} {...props}>
       {label || children}
