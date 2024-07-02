db67ff9b5964ae0650fb9be678a9feba140f511f
ITO_MZ_CHOESEUN\mz_choeseunghui <mz_choeseunghui@woowafriends.com>
2024-07-02 11:23:03 +0900

    FC, PropsWithChildren  예제

--- packages/react-zustand/src/components/TestComponents.tsx
+++ packages/react-zustand/src/components/TestComponents.tsx
@@ -1,4 +1,12 @@
-const TestComponents = (props) => {
+import { FC } from 'react';
+// import { PropsWithChildren  } from 'react';
+// FC<PropsWithChildren<props>>
+
+type props = {
+  title: string;
+};
+
+const TestComponents: FC<props> = (props) => {
   return <div>{props.title} 테스트 컴포넌트</div>;
 };
 
