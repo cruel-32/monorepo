b326a5706a89bf2850c529b8dd57b9bde9cf2a4b
ITO_MZ_CHOESEUN\mz_choeseunghui <mz_choeseunghui@woowafriends.com>
2024-07-29 15:02:54 +0900

    zustand

--- packages/common/src/components/atoms/Button.tsx
+++ packages/common/src/components/atoms/Button.tsx
@@ -9,7 +9,7 @@ export const Button: FC<ButtonProps> = ({ className, children, ...props }) => {
     <HButton
       type="button"
       className={classnames(
-        'rounded border border-transparent text-white bg-blue-600 hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-blue-200',
+        'rounded pt-2 pb-2 pl-4 pr-4 border-none text-white bg-blue-600 hover:bg-blue-700 focus:ring-1 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-blue-200 box-border',
         className,
       )}
       {...props}
