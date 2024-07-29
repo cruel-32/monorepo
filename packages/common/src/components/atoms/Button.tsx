ac3102cc1f88576032bd652629961e66799d6910
ITO_MZ_CHOESEUN\mz_choeseunghui <mz_choeseunghui@woowafriends.com>
2024-07-29 14:33:43 +0900

    tailwind

--- packages/common/src/components/atoms/Button.tsx
+++ packages/common/src/components/atoms/Button.tsx
@@ -4,12 +4,16 @@ import classnames from 'classnames';
 import { Button as HButton, ButtonProps } from '@headlessui/react';
 export type { ButtonProps } from '@headlessui/react';
 
-const customClassName =
-  'border border-transparent text-white bg-blue-600 hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-blue-200';
-
 export const Button: FC<ButtonProps> = ({ className, children, ...props }) => {
   return (
-    <HButton type="button" className={classnames(customClassName, className)} {...props}>
+    <HButton
+      type="button"
+      className={classnames(
+        'rounded border border-transparent text-white bg-blue-600 hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-blue-200',
+        className,
+      )}
+      {...props}
+    >
       {children}
     </HButton>
   );
